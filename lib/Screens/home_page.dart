be2c9b9ad492a138b4ea4211a0d91a115fa6205a
Sanskar Modi <modisanskar5@gmail.com>
2021-10-23 23:54:55 +0530

    Home Page added

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:home_electricity/Screens/individual_screen.dart';
+import 'package:sizer/sizer.dart';
+
+import 'profile.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -8,7 +15,232 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  List<String> Rooms = [
+    "assets/bed.svg",
+    "assets/room.svg",
+    "assets/kitchen.svg",
+    "assets/bathtube.svg",
+    "assets/house.svg",
+    "assets/balcony.svg",
+  ];
+  List<String> RoomNames = [
+    "Bedroom",
+    "Living Room",
+    "Kitchen",
+    "Bathroom",
+    "Outdoor",
+    "Balcony",
+  ];
+  List<String> Lights = [
+    "4",
+    "2",
+    "5",
+    "1",
+    "5",
+    "2",
+  ];
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset("assets/bulb.svg"), label: "Bulb"),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset("assets/Icon feather-home.svg"),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset("assets/Icon feather-settings.svg"),
+              label: "Home")
+        ],
+      ),
+      backgroundColor: Color.fromRGBO(10, 77, 162, 1),
+      body: SingleChildScrollView(
+        child: Container(
+          height: 100.h,
+          width: 100.w,
+          child: Stack(
+            children: [
+              Positioned(
+                bottom: 450,
+                left: 0,
+                right: 1,
+                child: SvgPicture.asset(
+                  "assets/Circles.svg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 3.5.h, 0, 0),
+                child: Container(
+                  width: 100.w,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            width: 30.w,
+                            child: Text(
+                              "Control Panel",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 27.sp),
+                              maxLines: 2,
+                            ),
+                          ),
+                          InkWell(
+                            onTap: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Profile()));
+                            },
+                            child: CircleAvatar(
+                              radius: 38,
+                              child: Image.asset("assets/avatar.png"),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 1.h,
+                      ),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(5.w, 2.h, 0, 0),
+                                child: Text(
+                                  "All  Rooms",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(10, 77, 162, 1),
+                                    fontSize: 20.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              MediaQuery.removePadding(
+                                context: context,
+                                removeTop: true,
+                                child: Expanded(
+                                  child: GridView.count(
+                                    scrollDirection: Axis.vertical,
+                                    shrinkWrap: true,
+                                    physics: ScrollPhysics(),
+
+                                    crossAxisCount: 2,
+                                    // Generate 100 widgets that display their index in the List.
+                                    children: List.generate(
+                                      6,
+                                      (index) {
+                                        return InkWell(
+                                          onTap: () {
+                                            Navigator.push(
+                                              context,
+                                              FadeRoute(
+                                                page: Individual(light: Lights[index],Room: RoomNames[index],),
+                                              ),
+                                            );
+                                          },
+                                          child: Center(
+                                            child: Card(
+                                              elevation: 2,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                              ),
+                                              color: Colors.white,
+                                              child: Container(
+                                                height: 18.h,
+                                                width: 42.w,
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(30),
+                                                ),
+                                                child: Padding(
+                                                  padding: EdgeInsets.fromLTRB(
+                                                      3.w, 0, 0, 0),
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceEvenly,
+                                                    children: [
+                                                      SvgPicture.asset(
+                                                        Rooms[index],
+                                                        height: 60,
+                                                        width: 60,
+                                                      ),
+                                                      Text(
+                                                        RoomNames[index],
+                                                        style: TextStyle(
+                                                          color: Color.fromRGBO(
+                                                              10, 77, 162, 1),
+                                                          fontSize: 18.sp,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                        ),
+                                                      ),
+                                                      Text(
+                                                        "${Lights[index]} Lights",
+                                                        style: TextStyle(
+                                                          color: Colors.orange,
+                                                          fontSize: 14.sp,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class FadeRoute extends PageRouteBuilder {
+  final Widget page;
+
+  FadeRoute({required this.page})
+      : super(
+          pageBuilder: (context, animation, anotherAnimation) => page,
+          transitionDuration: Duration(seconds: 5),
+          reverseTransitionDuration: Duration(milliseconds: 200),
+          transitionsBuilder: (context, animation, anotherAnimation, child) {
+            animation = CurvedAnimation(
+                curve: Curves.fastLinearToSlowEaseIn,
+                parent: animation,
+                reverseCurve: Curves.fastOutSlowIn);
+            return FadeTransition(
+              opacity: animation,
+              child: page,
+            );
+          },
+        );
 }
